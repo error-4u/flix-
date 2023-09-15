5ad3cdf4c27d065ec9d8f228003ad1173359530a
singh1475 <vboxuser@ubuntu1.myguest.virtualbox.org>
2023-09-15 13:03:08 +0530

    styled Signup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,8 +2,17 @@ function Signup(){
 
 
     return <div>
+        <center>
+            <div>Welcome to Coursera. Sign Up</div>
     <div style={{
-        border: "2px solid black"
+        border: "2px solid black",
+        width: 350,
+        marginTop: 200,
+        height: 200,
+      
+    
+
+    
     }}>
      username - <input type = {"text"}></input>
      <br />
@@ -11,6 +20,7 @@ function Signup(){
      <br />
      <button>Sign up</button>
      </div>
+     </center>
 
     </div>
 
