cb9a75515fdba711aed02b970e67a57dccd7aea4
singh1475 <vboxuser@ubuntu1.myguest.virtualbox.org>
2023-10-28 10:38:39 +0530

    conditional rendering in appbar & courses.map

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -1,78 +1,137 @@
+/* eslint-disable react/jsx-no-undef */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
-import { Typography } from "@mui/material";
-import { Button } from "@mui/material";
+//import { Typography } from "@mui/material";
+//import { Button } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography, Button } from '@mui/material';
 import { useEffect, useState } from "react";
+//import Toolbar from "@mui/material";
+//import Box from "@mui/material";
 import { useNavigate } from "react-router-dom";
  
 
 
-function Appbar(){
-
+export default function Header() {
   const navigate = useNavigate();
-  const [userEmail, setUseremail] = useState(null);
-
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [username, setUsername] = useState('');
 
   useEffect(() => {
-     
-    function callback2(data){
- 
-     if(data.userEmail){
-      setUseremail(data.userEmail);
-      console.log(userEmail);
+    fetch( 'http://localhost:3000/admin/me', { headers: { "Authorization": "Bearer " + localStorage.getItem("token") }})
+      .then(response => response.json())
+      .then(data => {
+        setUsername(data.username);
+        setLoggedIn(true);
+      })
+      .catch(error => {
+        console.error('Error fetching username:', error);
+        setLoggedIn(false);
+      });
+  }, []);
 
-     }
-    }
-    function callback1(res){
-      res.json().then(callback2)
-    }
-    fetch("http://localhost:3000/admin/me" , {
-      method: "GET",
-      headers : {
-       "authorization" : "Bearer " + localStorage.getItem("token")
-      }
-    }).then(callback1)
+  const handleLogin = () => {
+    navigate('/login');
+  };
 
+  const handleSignup = () => {
+    navigate('/signup');
+  };
 
-  },[]);
+  const handleLogout = () => {
+    // Perform logout logic here
+    localStorage.setItem("token", null);
+    setLoggedIn(false);
+    setUsername('');
+  };
 
-  if(userEmail){
-    return <div style={{display: "flex",
-    justifyContent: "space-between", padding: "1rem"}}>
-    
-    <div style={{backgroundColor: "red"}}>
-    <Typography variant="h5">Coursera</Typography>
-    </div>
-    <div>
-      {userEmail}
-    </div>
-    <div>
-   
-    <Button variant="contained" onClick={() => {
-      localStorage.setItem("token", null);
-      window.location="/";
-    }} >Logout</Button>
-    </div>
-    </div>
-  }
-
-return <div style={{display: "flex",
-justifyContent: "space-between", padding: "1rem"}}>
-
-<div style={{backgroundColor: "red"}}>
-<Typography variant="h5">Coursera</Typography>
-</div>
-<div>
-<Button variant="contained" onClick={() => {
-  navigate("/signup")
-}} style={{marginRight: 5}}>Sign up</Button>
-<Button variant="contained" onClick={() => {
-  navigate("/login")
-}} >Sign in</Button>
-</div>
-</div>
-
-
+  return (
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="h6" sx={{ flexGrow: 1 }}>Coursera </Typography>
+          {loggedIn ? (
+            <>
+              <Typography variant="subtitle1" component="span" sx={{ marginRight: '1rem' }}> {username} </Typography>
+              <Button color="inherit" onClick={handleLogout}> Logout </Button>
+            </>
+          ) : (
+            <>
+              <Button color="inherit" onClick={handleLogin}> Login </Button>
+              <Button color="inherit" onClick={handleSignup}> Signup </Button>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
 
-export default Appbar;
+// function Appbar(){
+
+//   const navigate = useNavigate();
+//   const [userEmail, setUseremail] = useState(null);
+
+
+//   useEffect(() => {
+     
+//     function callback2(data){
+ 
+//      if(data.userEmail){
+//       setUseremail(data.userEmail);
+//       console.log(userEmail);
+
+//      }
+//     }
+//     function callback1(res){
+//       res.json().then(callback2)
+//     }
+//     fetch("http://localhost:3000/admin/me" , {
+//       method: "GET",
+//       headers : {
+//        "authorization" : "Bearer " + localStorage.getItem("token")
+//       }
+//     }).then(callback1)
+
+
+//   },[]);
+
+//   if(userEmail){
+//     return <div style={{display: "flex",
+//     justifyContent: "space-between", padding: "1rem"}}>
+    
+//     <div style={{backgroundColor: "red"}}>
+//     <Typography variant="h5">Coursera</Typography>
+//     </div>
+//     <div>
+//       {userEmail}
+//     </div>
+//     <div>
+   
+//     <Button variant="contained" onClick={() => {
+//       localStorage.setItem("token", null);
+//       window.location="/";
+//     }} >Logout</Button>
+//     </div>
+//     </div>
+//   }
+
+// return <div style={{display: "flex",
+// justifyContent: "space-between", padding: "1rem"}}>
+
+// <div style={{backgroundColor: "red"}}>
+// <Typography variant="h5">Coursera</Typography>
+// </div>
+// <div>
+// <Button variant="contained" onClick={() => {
+//   navigate("/signup")
+// }} style={{marginRight: 5}}>Sign up</Button>
+// <Button variant="contained" onClick={() => {
+//   navigate("/login")
+// }} >Sign in</Button>
+// </div>
+// </div>
+
+
+// }
+
+// export default Appbar;
--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,4 +1,12 @@
+/* eslint-disable react/jsx-no-undef */
 /* eslint-disable no-unused-vars */
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import React, { useEffect, useState } from 'react'
 
 function Courses() {
@@ -6,7 +14,8 @@ function Courses() {
 
     useEffect(() => {
        function callback2(data){
-        console.log(data)
+        console.log(data.courses);
+        setCourses(data.courses);
 
        }
        function callback1(res){
@@ -23,9 +32,41 @@ function Courses() {
   return (
     <div>
         courses
+        {courses.map(course => {
+         return <Course course={course}/> }
+         
+
+        )}
       
     </div>
   )
 }
 
-export default Courses
+function Course(props){
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image=""
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Course card
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+       {props.course.title}
+       <br />
+       {props.course.description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+
+export default Courses;
