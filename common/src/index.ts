b059b77c7f3b1f672c4b639517955bbc48b902c0
error-4u <kumarvishal1082002@gmail.com>
2024-01-27 12:35:19 +0530

    z.infer

---
+++ common/src/index.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const loginInput = z.object({
+    username: z.string(),
+    password: z.string(),
+});
+
+export  type LoginParams = z.infer<typeof loginInput>;
+
+
+
+
+
+
+
