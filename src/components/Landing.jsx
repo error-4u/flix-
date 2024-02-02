e46ace72092752c0249cd2b03ff768ae0b6ab39b
error-4u <kumarvishal1082002@gmail.com>
2024-02-02 13:54:43 +0530

    style

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { Typography } from '@mui/material'
 import React from 'react'
+import "./css/landing.css"
 
 function Landing() {
   return (
@@ -11,7 +12,7 @@ function Landing() {
       <Typography >
       <h1 style={{marginTop:"1px"}}>Best Online Education Expertise</h1>
       </Typography>
-      <span>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.
+      <span>Far far away, behind the word mountains, far from the countries Vokalia and <br /> Consonantia, there live the blind texts.
       </span>
       </div>
      
@@ -23,8 +24,8 @@ function Landing() {
      </div>
      <div>
      <center>
-      <div style={{alignItems:"center", marginTop:"50px"}}>
-      <h1 style={{color:"blue"}}>Learn from 275+ leading universities and companies with Coursera Plus</h1>
+      <div style={{alignItems:"center", marginTop:"150px"}}>
+      <h1 style={{color:"#4e0eff"}}>Learn from 275+ leading universities and companies with Coursera Plus</h1>
       </div>
       </center>
       <div  style={{marginTop:"50px", alignItems:"baseline"}}>
@@ -39,16 +40,75 @@ function Landing() {
           </center>
       </div>
       </div>
-      <div style={{display:"flex", marginTop:"80px", justifyContent:"space-around"}}>
+      <div style={{display:"flex", marginTop:"80px", justifyContent:"space-around", alignContent:"center"}}>
         
       <img src="src/assets/sitting_girl.webp" alt="" srcset="" style={{height:"1000px", width:"100%", marginLeft:"0", paddingLeft:"0px"}}/>
+      {/* <div style={{minWidth:"1000px"}}>
+       
       <div style={{fontSize:"46px", fontWeight:"500", lineHeight:"1,4", padding:"20px"}}>
-        <h2 style={{color:"black"}}>Benefits About Online Learning Expertise </h2>
+        <div style={{color:"#4e0eff",fontSize:"16px", fontWeight:"700"}}>LEARN ANYTHING</div>
+        <h2 style={{color:"black", marginTop:"1px",whiteSpace:"nowrap", marginRight:"220px" }}>Benefits About Online <br />Learning Expertise </h2>
+     
+        <div style={{fontSize:"20px", fontWeight:"500", fontFamily:"Roboto"}}>Online Courses</div>
+        <p>Far far away, behind the word mountains, far from the <br /> countries Vokalia and Consonantia, there live the blind texts.</p>
+        </div> 
+        </div>  */}
+
+<div className="col-md-6 heading-section d-flex align-items-center"
+         >
+      <div className="mt-0 my-lg-5 py-5">
+        <span className="subheading">Learn Anything</span>
+        <h2 className="mb-2">Benefits About Online Learning Expertise</h2>
+        <div className="row mt-4 g-lg-2">
+          <div className="col-lg-12 d-flex align-items-stretch services-wrap">
+            <div className="services d-flex">
+              <div className="icon">
+                <span className="flaticon-online-learning"></span>
+              </div>
+              <div className="text">
+                <h2>Online Courses</h2>
+                <p style={{ marginBottom: 0 }}>
+                  Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                  there live the blind texts.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-12 d-flex align-items-stretch services-wrap">
+            <div className="services d-flex">
+              <div className="icon">
+                <span className="flaticon-certificate"></span>
+              </div>
+              <div className="text">
+                <h2>Earn A Certificates</h2>
+                <p style={{ marginBottom: 0 }}>
+                  Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                  there live the blind texts.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-12 d-flex align-items-stretch services-wrap">
+            <div className="services d-flex">
+              <div className="icon">
+                <span className="flaticon-scientist"></span>
+              </div>
+              <div className="text">
+                <h2>Learn with Expert</h2>
+                <p style={{ marginBottom: 0 }}>
+                  Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                  there live the blind texts.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-
-        
-
       </div>
+    </div>
+    </div>
+
+    
+
    
     </div>
   )
