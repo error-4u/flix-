23a93161c68d200e8ce37c3932a286bad9f62ffa
error-4u <kumarvishal1082002@gmail.com>
2024-02-24 19:46:33 +0530

    tailwindcss

--- client/src/components/Appbar.jsx
+++ client/src/components/Appbar.jsx
@@ -5,22 +5,18 @@
 /* eslint-disable no-unused-vars */
 //import { Typography } from "@mui/material";
 //import { Button } from "@mui/material";
-import { AppBar, Box, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Box, Toolbar, Typography, Button } from "@mui/material";
 import { useEffect, useState } from "react";
 //import Toolbar from "@mui/material";
 //import Box from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import { useRecoilState, useRecoilValue, useSetRecoilState} from 'recoil';
-import { userState } from '../store/atoms/user';
-import { userEmailState } from '../store/selectors/userEmail';
-import { isUserLoading } from '../store/selectors/isUserLoading';
-
- 
-
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { userState } from "../store/atoms/user";
+import { userEmailState } from "../store/selectors/userEmail";
+import { isUserLoading } from "../store/selectors/isUserLoading";
 
 export default function Appbar() {
   const navigate = useNavigate();
-
 
   // THESE TWO VARIABLE ARE SELECTORS
   const userLoading = useRecoilValue(isUserLoading);
@@ -28,143 +24,148 @@ export default function Appbar() {
 
   const setUser = useSetRecoilState(userState);
 
-
-  if(userLoading){
-    return <>loading.......</>
+  if (userLoading) {
+    return <>loading.......</>;
   }
   const handleLogin = () => {
-    navigate('/login');
+    navigate("/login");
   };
 
   const handleSignup = () => {
-    navigate('/signup');
+    navigate("/signup");
   };
 
   const handleLogout = () => {
     // Perform logout logic here
     localStorage.setItem("token", null);
-   setUser({
-    isLoading: false,
-    userEmail: null
-   })
+    setUser({
+      isLoading: false,
+      userEmail: null,
+    });
   };
 
   // if(userEmail){
-    if (userEmail) {
-      return <div style={{
+  if (userEmail) {
+    return (
+      <div
+        style={{
           display: "flex",
           justifyContent: "space-between",
           padding: 4,
-          zIndex: 1
-      }}>
-          <div style={{marginLeft: 10, cursor: "pointer", color:"white"}} onClick={() => {
-              navigate("/")
-          }}>
-              <Typography variant={"h5"}>Flix</Typography>
+          zIndex: 1,
+        }}
+      >
+        <div
+          style={{ marginLeft: 10, cursor: "pointer", color: "white" }}
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          <Typography variant={"h5"}>Flix</Typography>
+        </div>
+
+        <div style={{ display: "flex" }}>
+          <div style={{ marginRight: 10, display: "flex" }}>
+            <div style={{ marginRight: 10 }}>
+              <Button
+                variant="contained"
+                style={{ backgroundColor: "#4e0eff", color: "white" }}
+                onClick={() => {
+                  navigate("/user/courses");
+                }}
+              >
+                Courses
+              </Button>
+            </div>
+
+            <Button
+              variant={"contained"}
+              style={{ backgroundColor: "#4e0eff", color: "white" }}
+              onClick={() => {
+                localStorage.setItem("token", null);
+                setUser({
+                  isLoading: false,
+                  userEmail: null,
+                });
+                navigate("/");
+              }}
+            >
+              Logout
+            </Button>
           </div>
-  
-          <div style={{display: "flex"}}>
-              <div style={{marginRight: 10, display: "flex"}}>
-           
-
-                  <div style={{marginRight: 10}}>
-                      <Button variant='contained' style={{  backgroundColor: '#4e0eff',
-                            color: 'white',}}
-
-                          onClick={() => {
-                              navigate("/user/courses")
-                          }}
-                      >Courses</Button>
-                      
-                  </div>
-
-                  <Button
-                      variant={"contained"}
-
-                      style={{  backgroundColor: '#4e0eff',
-                            color: 'white',}}
-
-                            
-                      onClick={() => {
-                          localStorage.setItem("token", null);
-                          setUser({
-                              isLoading: false,
-                              userEmail: null
-                          })
-                          navigate("/")
-                      }}
-                  >Logout</Button>
-              </div>
-          </div>
+        </div>
       </div>
+    );
   } else {
-      return <div style={{
+    return (
+      <div
+        style={{
           display: "flex",
           justifyContent: "space-between",
           padding: 4,
-          zIndex: 1
-      }}>
-          <div style={{marginLeft: 10, cursor: "pointer", color:"white"}} onClick={() => {
-              navigate("/")
-          }}>
-              <Typography variant={"h4"}>Flix-</Typography>
-          </div>
-  
-          <div style={{display: "flex"}}>
-              <div style={{marginRight: 10}}>
-                  <Button
+          zIndex: 1,
+        }}
+      >
+        <div
+          className="mt-2 p-1"
+          style={{ marginLeft: 10, cursor: "pointer", color: "white" }}
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          <Typography variant={"h4"}>Flix-</Typography>
+        </div>
 
-              style={{  backgroundColor: "#4e0eff",
-              color: 'white',}}
-                      variant={"contained"}
-                      onClick={() => {
-                          navigate("/signup")
-                      }}
-                  >Signup</Button>
-              </div>
-              <div>
-                  <Button
-                        style={{  backgroundColor: '#4e0eff',
-                        color: 'white',}}
-
-                      variant={"contained"}
-                      onClick={() => {
-                          navigate("/login")
-                      }}
-                  >Signin</Button>
-              </div>
+        <div className="mr-10 mt-4 p-1" style={{ display: "flex" }}>
+          <div style={{ marginRight: 10 }}>
+            <Button
+              style={{ backgroundColor: "#4e0eff", color: "white" }}
+              variant={"contained"}
+              onClick={() => {
+                navigate("/signup");
+              }}
+            >
+              Signup
+            </Button>
           </div>
+          <div>
+            <Button
+              style={{ backgroundColor: "#4e0eff", color: "white" }}
+              variant={"contained"}
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              Signin
+            </Button>
+          </div>
+        </div>
       </div>
+    );
   }
 }
-  //     <Box>
-  //     <AppBar position="static">
-  //       <Toolbar>
-        
-          
-  //             <Typography variant="subtitle1" component="span" sx={{ marginRight: '1rem', backgroundColor:"red"}}> {username} </Typography>
-  //             <Button color="inherit" onClick={handleLogout}> Logout </Button>
-          
-          
-  //       </Toolbar>
-  //     </AppBar>
-  //   </Box>
+//     <Box>
+//     <AppBar position="static">
+//       <Toolbar>
 
-    
-  // }
+//             <Typography variant="subtitle1" component="span" sx={{ marginRight: '1rem', backgroundColor:"red"}}> {username} </Typography>
+//             <Button color="inherit" onClick={handleLogout}> Logout </Button>
 
- 
+//       </Toolbar>
+//     </AppBar>
+//   </Box>
+
+// }
 
 // function Appbar(){
 
 //   const navigate = useNavigate();
 //   const [userEmail, setUseremail] = useState(null);
 
-
 //   useEffect(() => {
-     
+
 //     function callback2(data){
- 
+
 //      if(data.userEmail){
 //       setUseremail(data.userEmail);
 //       console.log(userEmail);
@@ -181,13 +182,12 @@ export default function Appbar() {
 //       }
 //     }).then(callback1)
 
-
 //   },[]);
 
 //   if(userEmail){
 //     return <div style={{display: "flex",
 //     justifyContent: "space-between", padding: "1rem"}}>
-    
+
 //     <div style={{backgroundColor: "red"}}>
 //     <Typography variant="h5">Coursera</Typography>
 //     </div>
@@ -195,7 +195,7 @@ export default function Appbar() {
 //       {userEmail}
 //     </div>
 //     <div>
-   
+
 //     <Button variant="contained" onClick={() => {
 //       localStorage.setItem("token", null);
 //       window.location="/";
@@ -219,7 +219,6 @@ export default function Appbar() {
 // }} >Sign in</Button>
 // </div>
 // </div>
-
 
 // }
 
